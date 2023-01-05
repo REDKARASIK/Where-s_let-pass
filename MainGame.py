d288f7b8bfe6af706369a91c5c975d47121b530c
maver <maver2005@yandex.ru>
2023-01-05 17:52:00 +1000

    get_monitor_size

---
+++ MainGame.py
@@ -0,0 +1,17 @@
+from main_functions import *
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+c = 0
+if c == 0:
+    # start screen
+    pass
+if c == 1:
+    # choice level
+    pass
+if c == 2:
+    # start level
+    pass
+
+
