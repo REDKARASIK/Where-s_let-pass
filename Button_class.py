e3263d04e31c3bd5cb85a192d620a4763d02bfba
REDKARASIK <redkarasik@yandex.ru>
2023-01-05 18:12:29 +1000

    Starting screen develop Button class develop, not working

---
+++ Button_class.py
@@ -0,0 +1,38 @@
+import pygame
+import sys
+from main_functions import terminate
+
+pygame.init()
+size = width, height = 600, 400
+screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, text, pos_x, pos_y, *group):
+        super().__init__(*group)
+        self.text = text
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.step = 15
+        self.font = pygame.font.Font(None, 50)
+        self.text_m = self.font.render(self.text, True, 'white')
+        self.text_width = self.text_m.get_width()
+        self.text_height = self.text_m.get_height()
+        self.image = pygame.Surface([self.text_width + self.step * 2, self.text_height + self.step * 2])
+        self.rect = pygame.Rect(self.pos_x, self.pos_y, self.text_width + self.step * 2,
+                                self.text_height + self.step * 2)
+        pygame.draw.rect(self.image, 'white', (self.pos_x, self.pos_y, self.text_width + self.step * 2,
+                         self.text_height + self.step * 2), 2)
+
+
+if __name__ == '__main__':
+    running = True
+    button = Button('Привет', 100, 100, all_sprites)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        screen.fill('black')
+        all_sprites.draw(screen)
+        pygame.display.flip()
---
+++ starting_screen.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+import os
+import random
+
+pygame.init()
+
+
+def start_screen(screen, current_level):
+    intro_text = ["Where's let-pass?", '', 'Новая игра', 'Продолжить игру', 'Выйти из игры']
