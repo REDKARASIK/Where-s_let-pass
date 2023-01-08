d45c965a97429502315aae77c72bdfe633b47960
maver <maver2005@yandex.ru>
2023-01-08 20:24:25 +1000

    tiles added

---
+++ data/supertile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="supertile" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../2D Pixel Dungeon Asset Pack/character and tileset/imgonline-com-ua-Sharpen-XLkaqfwMgVpy.png" width="320" height="320"/>
+</tileset>
