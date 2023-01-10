93c50e56aaafc8a134f94c28d707bd76a9d90c95
REDKARASIK <redkarasik@yandex.ru>
2023-01-10 17:25:18 +1000

    player movement, problem with frames

--- player_class.py
+++ player_class.py
@@ -1,11 +1,10 @@
 import pygame
-import os
 from main_functions import load_image, terminate
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 all_sprites = pygame.sprite.Group()
-fps = 10
+fps = 15
 
 
 class Player(pygame.sprite.Sprite):
@@ -19,6 +18,8 @@ class Player(pygame.sprite.Sprite):
         self.image = self.idle_frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
         self.transform = False
+        self.speed = 40
+        self.walk_check = False
 
     def cut_sheet(self, frames, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -30,23 +31,29 @@ class Player(pygame.sprite.Sprite):
                     frame_location, self.rect.size)))
 
     def update(self, *args):
+        if self.walk_check:
+            self.cur_frame = (self.cur_frame + 1) % len(self.walk_frames)
+            self.image = self.walk_frames[self.cur_frame]
+            if self.transform:
+                self.image = pygame.transform.flip(self.image, True, False)
         if args[0][pygame.K_d]:
-            self.cur_frame = (self.cur_frame + 1) % len(self.walk_frames)
-            self.image = self.walk_frames[self.cur_frame]
-            self.rect.x += 20 / fps
+            self.rect.x += self.speed / fps
             self.transform = False
-        elif args[0][pygame.K_a]:
-            self.cur_frame = (self.cur_frame + 1) % len(self.walk_frames)
-            self.image = self.walk_frames[self.cur_frame]
-            self.image = pygame.transform.flip(self.image, True, False)
+        if args[0][pygame.K_a]:
             self.transform = True
-            self.rect.x -= 20 / fps
-
-        else:
+            self.rect.x -= self.speed / fps
+        if args[0][pygame.K_s]:
+            self.rect.y += self.speed / fps
+        if args[0][pygame.K_w]:
+            self.rect.y -= self.speed / fps
+        if not (args[0][pygame.K_w] or args[0][pygame.K_s] or args[0][pygame.K_a] or args[0][pygame.K_d]):
             self.cur_frame = (self.cur_frame + 1) % len(self.idle_frames)
             self.image = self.idle_frames[self.cur_frame]
             if self.transform:
                 self.image = pygame.transform.flip(self.image, True, False)
+            self.walk_check = False
+        else:
+            self.walk_check = True
 
 
 if __name__ == '__main__':
