d2f945116d874b7f2b60f4808af3c95438ef0ac5
REDKARASIK <redkarasik@yandex.ru>
2023-01-15 12:56:19 +1000

    Correct code, player class, add attack funcs

--- player_class.py
+++ player_class.py
@@ -6,12 +6,14 @@ from class_map import Map
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 all_sprites = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
 fps = 25
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, level, *groups):
+    def __init__(self, x, y, level, enemy_group, *groups):
         super().__init__(*groups)
+        self.enemy_group = enemy_group
         self.map_check = level
         self.idle_frames = []
         self.cut_sheet(self.idle_frames, load_image('main_idle.png'), 7, 1)
@@ -44,6 +46,24 @@ class Player(pygame.sprite.Sprite):
                 frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, (self.rect.width, self.rect.height))))
 
+    def attack_1(self):
+        self.image = self.fight_frames[self.cur_frame]
+        self.cur_frame = (self.cur_frame + 1) % len(self.fight_frames)
+        if self.transform:
+            self.image = pygame.transform.flip(self.image, True, False)
+        if not self.cur_frame:
+            self.attack = False
+            self.cur_frame = 0
+
+    def attack_2_func(self):
+        self.image = self.fight_frames_2[self.cur_frame]
+        self.cur_frame = (self.cur_frame + 1) % len(self.fight_frames_2)
+        if self.transform:
+            self.image = pygame.transform.flip(self.image, True, False)
+        if not self.cur_frame:
+            self.attack_2 = False
+            self.cur_frame = 0
+
     def update(self, *args):
         k = self.rect.width
         if args[0][pygame.K_f] and not self.attack_2:
@@ -51,25 +71,13 @@ class Player(pygame.sprite.Sprite):
             self.attack = True
             self.walk_check = False
         if self.attack:
-            self.image = self.fight_frames[self.cur_frame]
-            self.cur_frame = (self.cur_frame + 1) % len(self.fight_frames)
-            if self.transform:
-                self.image = pygame.transform.flip(self.image, True, False)
-            if not self.cur_frame:
-                self.attack = False
-                self.cur_frame = 0
+            self.attack_1()
         if args[0][pygame.K_g] and not self.attack:
             self.cur_frame = 0
             self.attack_2 = True
             self.walk_check = False
         if self.attack_2:
-            self.image = self.fight_frames_2[self.cur_frame]
-            self.cur_frame = (self.cur_frame + 1) % len(self.fight_frames_2)
-            if self.transform:
-                self.image = pygame.transform.flip(self.image, True, False)
-            if not self.cur_frame:
-                self.attack_2 = False
-                self.cur_frame = 0
+            self.attack_2_func()
         if self.walk_check:
             self.cur_frame = (self.cur_frame + 1) % len(self.walk_frames)
             self.image = self.walk_frames[self.cur_frame]
@@ -161,7 +169,7 @@ if __name__ == '__main__':
                              [6, 7, 8, 9, 16, 17, 18, 19, 26, 27, 28, 29, 60, 61, 62, 63, 70, 71, 72, 73, 11, 12, 13,
                               14, 21, 22,
                               23, 24, 31, 32, 33, 34, 41, 43])), 50)
-    player = Player(64, 64, map_level, all_sprites)
+    player = Player(64, 64, map_level, enemy_group, all_sprites)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
